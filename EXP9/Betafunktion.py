bb97912abb1d21604096494120a42cbe989444be
Mona Marie Eberenz <mm.eberenz@googlemail.com>
2026-09-09 14:15:39 +0200

    Update Betafunktion.py
    Achsenbeschriftung angepasst

--- EXP9/Betafunktion.py
+++ EXP9/Betafunktion.py
@@ -20,8 +20,8 @@ fig, ax = plt.subplots()
 for i in range(4):
     ax.errorbar(x_RMS[i], y_RMS[i], xerr=RMSerr, yerr=RMSerr, capsize=5, color=farben[i], label=f"Schirm {i+3}")
 
-ax.set_xlabel(r"$x_\mathrm{RMS}$ [mm]")
-ax.set_ylabel(r"$y_\mathrm{RMS}$ [mm]")
+ax.set_xlabel(r"$\sigma_\mathrm{x}$ [mm]")
+ax.set_ylabel(r"$\sigma_\mathrm{y}$ [mm]")
 ax.grid(True)
 ax.set_title("Strahlbreiten an Schirmen 3-6")
 ax.legend()
