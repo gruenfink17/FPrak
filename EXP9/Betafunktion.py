0f9ad31530c3c032adb7a6ce664290c31dcca18a
Mona Marie Eberenz <mm.eberenz@googlemail.com>
2026-09-09 14:14:19 +0200

    Update Betafunktion.py
    RMS Fehler aus Quadrupolscan übernommen

--- EXP9/Betafunktion.py
+++ EXP9/Betafunktion.py
@@ -8,19 +8,17 @@ Created on Wed Sep  9 13:57:43 2026
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Fehler?? 
-
-
 z_schirm = np.array([176, 232.5, 288.5, 344.5])
 x_RMS = np.array([2.872729, 4.045438, 3.033364, 2.569578])
 y_RMS = np.array([2.473640, 3.788590, 3.564704, 1.235016])
+RMSerr = 0.2/np.sqrt(15) #Fehler auf Strahlbreiten in mm
 
 farben = ['red', 'blue', 'green', 'orange']
 
 fig, ax = plt.subplots()
 
 for i in range(4):
-    ax.scatter(x_RMS[i], y_RMS[i], color=farben[i], label=f"Schirm {i+3}")
+    ax.errorbar(x_RMS[i], y_RMS[i], xerr=RMSerr, yerr=RMSerr, capsize=5, color=farben[i], label=f"Schirm {i+3}")
 
 ax.set_xlabel(r"$x_\mathrm{RMS}$ [mm]")
 ax.set_ylabel(r"$y_\mathrm{RMS}$ [mm]")
